e0113f0590bb8d065a57d1f9e5fbb344c60d75a0
lb <lbsystem1126@gmail.com>
2023-09-07 18:26:51 +0800

    fix rawConn and Encrypto

--- encrypt/xor.go
+++ encrypt/xor.go
@@ -1,36 +1,34 @@
 package encrypt
 
-type MyEncrypto struct{
-	Key []byte
-	
+import "crypto/sha512"
+
+type MyEncrypto struct {
+	Key  []byte
+	Temp []byte
 }
 
+func (e *MyEncrypto) NewEncrypto(key []byte) *MyEncrypto {
+	hash := sha512.New()
+	hash.Write(key)
+	hash.Write([]byte("fdas19"))
+	return &MyEncrypto{Key: hash.Sum(nil)}
+}
 
-func (e *MyEncrypto) XorIv(Nonce []byte ){
+func (e *MyEncrypto) XorIv(Nonce []byte) {
 	encrypted := make([]byte, len(e.Key))
 	keyLen := len(Nonce)
 	for i := range e.Key {
 		encrypted[i] = e.Key[i] ^ Nonce[i%keyLen]
 	}
-	e.Key=encrypted
+	e.Temp = encrypted
 }
 
 func (e *MyEncrypto) XorCipher(data []byte) []byte {
-	packetLength := len(data)
-	packetLengthByte := byte(packetLength) // 包长转成ASCII字符
 
-	// 用包长改变密钥
-	modifiedKey := make([]byte, len(e.Key))
-	for i, keyByte := range e.Key {
-		modifiedKey[i] = keyByte ^ packetLengthByte
-	}
-
-	// 用修改后的密钥进行XOR加密
-	encrypted := make([]byte, packetLength)
-	keyLen := len(modifiedKey)
+	encrypted := make([]byte, len(data))
+	keyLen := len(e.Temp)
 	for i := range data {
-		j := i % keyLen
-		encrypted[i] = data[i] ^ modifiedKey[j]
+		encrypted[i] = data[i] ^ e.Temp[i%keyLen]
 	}
 	return encrypted
 }
--- udp/rawConn/rawConn.go
+++ udp/rawConn/rawConn.go
@@ -1,18 +1,16 @@
 package rawConn
 
-//data 2
 import (
 	"encoding/binary"
 	"errors"
 	"fmt"
+	"github.com/lbsystem/my-go-mod/encrypt"
+	"github.com/lbsystem/my-go-mod/udp/codec"
+	"golang.org/x/sys/unix"
 	"log"
 	"net"
 	"strconv"
 	"time"
-	"crypto/sha512"
-	"github.com/lbsystem/my-go-mod/udp/codec"
-
-	"golang.org/x/sys/unix"
 
 	"golang.org/x/net/bpf"
 	"golang.org/x/net/ipv4"
@@ -20,32 +18,29 @@ import (
 
 type RawPackConn struct {
 	*ipv4.RawConn
-	ipHeader *ipv4.Header
-	srcIP    *net.UDPAddr
-	fd       int
-
+	ipHeader    *ipv4.Header
+	srcIP       *net.UDPAddr
+	fd          int
+	nonce       []byte
+	encryptMode bool
 }
-var Key []byte
-var 	EnCrypto bool	
+
 func (c *RawPackConn) Close() error {
 	return unix.Close(c.fd)
 }
 
-func (c *RawPackConn) SetEnCrypto(key []byte){
-	h := sha512.New()
-	h.Write(key)
-	h.Write([]byte("hijoadf|~js*(%)io4!!@#"))
-	Key=h.Sum(nil)
-	EnCrypto=true
-}
+var en *encrypt.MyEncrypto
 
 func (c *RawPackConn) ReadFrom(b []byte) (int, net.Addr, error) {
 	h, newByte, _, err := c.RawConn.ReadFrom(b)
 	if err != nil {
 		return 0, nil, err
 	}
-	
-	n := copy(b, newByte[8:])
+	iv := make([]byte, 2)
+	binary.LittleEndian.PutUint16(iv, uint16(h.ID))
+	en.XorIv(iv)
+	newByte = en.XorCipher(newByte[8:])
+	n := copy(b, newByte)
 	return n, &net.UDPAddr{
 		IP:   h.Src,
 		Port: int(binary.BigEndian.Uint16(newByte[0:2])),
@@ -57,9 +52,16 @@ func (c *RawPackConn) WriteTo(b []byte, dstaddr net.Addr) (int, error) {
 	if !ok {
 		return 0, errors.New("dstaddr is not a net.UDPAddr")
 	}
+	ipId := codec.GenerateRandomPort()
 	packetLen := len(b)
+	if c.encryptMode {
+		binary.LittleEndian.PutUint16(c.nonce, uint16(ipId))
+		en.XorIv(c.nonce)
+		b = en.XorCipher(b)
+	}
+
 	c.ipHeader.Dst = dstAdrr.IP
-	c.ipHeader.ID = codec.GenerateRandomPort()
+	c.ipHeader.ID = ipId
 	c.ipHeader.TotalLen = 20 + 8 + packetLen
 	b = codec.BuildUDPPacket(c.srcIP, dstAdrr, b)
 	err := c.RawConn.WriteTo(c.ipHeader, b, nil)
@@ -101,7 +103,10 @@ func (c *RawPackConn) UnixWriteTo(b []byte, dstaddr net.Addr) (int, error) {
 	c.ipHeader.ID = codec.GenerateRandomPort()
 	c.ipHeader.TotalLen = 20 + 8 + packetLen
 	b = codec.BuildUDPPacket(c.srcIP, dstAdrr, b)
-	hb, _ := c.ipHeader.Marshal()
+	hb, err := c.ipHeader.Marshal()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 	finalyB := append(hb, b...)
 	sockAddr := UDPAddrToSockaddr(dstAdrr)
 	if sockAddr == nil {
@@ -110,29 +115,6 @@ func (c *RawPackConn) UnixWriteTo(b []byte, dstaddr net.Addr) (int, error) {
 
 	return packetLen, unix.Sendto(c.fd, finalyB, 0, sockAddr)
 }
-
-func (c *RawPackConn) UnixWriteToEnCryto(b []byte, dstaddr net.Addr) (int, error) {
-	dstAdrr, ok := dstaddr.(*net.UDPAddr)
-	if !ok {
-		return 0, errors.New("dstaddr is not a net.UDPAddr")
-	}
-	packetLen := len(b)
-	c.ipHeader.Dst = dstAdrr.IP
-	c.ipHeader.ID = codec.GenerateRandomPort()
-	c.ipHeader.TotalLen = 20 + 8 + packetLen
-	b=XorCipher(b)
-	b = codec.BuildUDPPacket(c.srcIP, dstAdrr, b)
-	hb, _ := c.ipHeader.Marshal()
-	
-	finalyB := append(hb, b...)
-	sockAddr := UDPAddrToSockaddr(dstAdrr)
-	if sockAddr == nil {
-		return 0, errors.New("Failed to convert net.UDPAddr to unix.Sockaddr")
-	}
-
-	return packetLen, unix.Sendto(c.fd, finalyB, 0, sockAddr)
-}
-
 
 type Filter []bpf.Instruction
 
@@ -144,7 +126,12 @@ func setFDNonblocking(fd int) error {
 	return nil
 }
 
-func NewRawConn(addr string, port int, setNonblocking bool) *RawPackConn {
+func NewRawConn(addr string, port int, setNonblocking, setEnCrypto bool) *RawPackConn {
+
+	if setEnCrypto {
+		en = &encrypt.MyEncrypto{Key: []byte("1234567890abcdef")}
+	}
+
 	filter := Filter{
 		bpf.LoadAbsolute{Off: 22, Size: 2},                               // load the destination port
 		bpf.JumpIf{Cond: bpf.JumpEqual, Val: uint32(port), SkipFalse: 1}, // if Val != 8972 skip next instruction
@@ -161,6 +148,7 @@ func NewRawConn(addr string, port int, setNonblocking bool) *RawPackConn {
 
 	fd, err := unix.Socket(unix.AF_INET, unix.SOCK_RAW, unix.SOCK_DGRAM)
 	if err != nil {
+		fmt.Println("create fd is ")
 		panic(err)
 	}
 	// 设置接收缓冲区大小为 4096 字节
@@ -198,16 +186,7 @@ func NewRawConn(addr string, port int, setNonblocking bool) *RawPackConn {
 		TTL:      64,
 		Protocol: 17,
 		Src:      net.ParseIP(addr),
-	}, srcIPandPort, int(fd)}
-}
-
-func XorCipher(data []byte) []byte {
-	encrypted := make([]byte, len(data))
-	keyLen := len(Key)
-	for i := range data {
-		encrypted[i] = data[i] ^ Key[i%keyLen]
-	}
-	return encrypted
+	}, srcIPandPort, int(fd), make([]byte, 2), setEnCrypto}
 }
 
 type PacketConn interface {
