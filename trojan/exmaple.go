63a64e312c17ab1b28e51eb20f12f2d5a7885663
lb <lbsystem1126@gmail.com>
2023-11-27 16:23:05 +0800

    fix

--- trojan/exmaple.go
+++ trojan/exmaple.go
@@ -1,37 +1,37 @@
-package trojan
+// package trojan
 
-import (
-	"fmt"
+// import (
+// 	"fmt"
 
-	"github.com/p4gefau1t/trojan-go/tunnel"
-)
+// 	"github.com/p4gefau1t/trojan-go/tunnel"
+// )
 
-func Test() {
-	trcfg := MyTrojanCfg{
-		ServerAddr:       "kacb97.gkkgrp.xyz",
-		ServerPort:       24113,
-		ServerSNI:        "0gr4uqmtt8y41hcjsgrzdrc31.ourdvsss.com",
-		DisableHTTPCheck: false,
-		MuxOpen:          false,
-		MuxLimit:         16,
-		ServerPassword:   "xxxxxxxxxxxxxxxxxxxxxxxxx",
-	}
-	mt := NewTrojan(trcfg)
-	tcp, err := mt.DialConn(&tunnel.Address{
-		DomainName:  "xxxxxxxxxxxxxxxxx",
-		AddressType: tunnel.DomainName,
-		Port:        33326,
-	})
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	tcp.Write([]byte("fasdfaasdfasd"))
-	fmt.Printf("tcp.RemoteAddr(): %v\n", tcp.Metadata().Address)
-	mt.Udp.WriteWithMetadata([]byte("1111111"), &tunnel.Metadata{
-		Address: &tunnel.Address{
-			DomainName:  "xxxxxxxxx",
-			AddressType: tunnel.DomainName,
-			Port:        33326,
-		},
-	})
-}
+// func Test() {
+// 	trcfg := MyTrojanCfg{
+// 		ServerAddr:       "kacb97.gkkgrp.xyz",
+// 		ServerPort:       24113,
+// 		// ServerSNI:        "0gr4uqmtt8y41hcjsgrzdrc31.ourdvsss.com",
+// 		DisableHTTPCheck: false,
+// 		MuxOpen:          false,
+// 		MuxLimit:         16,
+// 		ServerPassword:   "xxxxxxxxxxxxxxxxxxxxxxxxx",
+// 	}
+// 	mt := NewTrojan(trcfg)
+// 	tcp, err := mt.DialConn(&tunnel.Address{
+// 		DomainName:  "xxxxxxxxxxxxxxxxx",
+// 		AddressType: tunnel.DomainName,
+// 		Port:        33326,
+// 	})
+// 	if err != nil {
+// 		fmt.Println(err.Error())
+// 	}
+// 	tcp.Write([]byte("fasdfaasdfasd"))
+// 	fmt.Printf("tcp.RemoteAddr(): %v\n", tcp.Metadata().Address)
+// 	mt.Udp.WriteWithMetadata([]byte("1111111"), &tunnel.Metadata{
+// 		Address: &tunnel.Address{
+// 			DomainName:  "xxxxxxxxx",
+// 			AddressType: tunnel.DomainName,
+// 			Port:        33326,
+// 		},
+// 	})
+// }
