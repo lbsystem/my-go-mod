0243d70742ac99a7f4e2610c4ec72a6bd9776440
lbsystem <42770927+lbsystem@users.noreply.github.com>
2024-03-23 12:15:42 +0800

    test vscode

--- test_struct.go
+++ test_struct.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	fmt.Printf("ok")
 	cd := gfw.NewDomains("https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/gfw.txt")
 	fmt.Printf("cd.IsSubdomainOfAny(\"www.youtube.com\"): %v\n", cd.IsSubdomainOfAny("www.qq.com"))
 }
